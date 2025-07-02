d344ae4300663315d32e881d8ae3ff39a9401f5e
Dante Schacht <dantesch10@gmail.com>
2025-07-01 20:40:55 -0400

    Se agrego la prueba con junit

---
+++ Ventas/src/test/java/com/Ventas/services/VentaServiceTest.java
@@ -0,0 +1,30 @@
+package com.Ventas.services;
+
+import org.junit.jupiter.api.Test;
+
+public class VentaServiceTest {
+    @Test
+    void testActualizar() {
+
+    }
+
+    @Test
+    void testCrear() {
+
+    }
+
+    @Test
+    void testEliminar() {
+
+    }
+
+    @Test
+    void testObtenerPorId() {
+
+    }
+
+    @Test
+    void testObtenerTodas() {
+
+    }
+}
